6c671f2c331300b5e9f530485be0aa364eae09f7
Haozhe Qi <hqi@todi-ln004.cscs.ch>
2024-10-16 07:53:18 +0200

    add todi runfile, delete some old runfiles

--- run_EK100.sh
+++
@@ -1,9 +0,0 @@
-python3 action/dataset.py \
-    --root /media/data/haozhe/VFM/onevision/llava_video/EK100 \
-    --train-metadata /media/data/haozhe/VFM/EK100/epic-kitchens-100-annotations/EPIC_100_train.csv \
-    --val-metadata /media/data/haozhe/VFM/EK100/epic-kitchens-100-annotations/EPIC_100_validation.csv \
-    --llm_size 0.5b \
-    --llava_num_frames 32 --clip-length 32 \
-    --llava_checkpoint experiments/EK100_test_new \
-    --action_predictions /media/data/haozhe/VFM/EK100/predictions.json \
-    --topk_predictions 10 > EK100_test_new.out 2>&1
--- shaokai_generate_train.sh
+++ shaokai_generate_train.sh
@@ -6,9 +6,9 @@
 #   > train_gen.out 2>&1
 
 python3 action/generate_description.py \
-   --train_metadata /storage-rcp-pure/upmwmathis_scratch/shaokai/epic-kitchens-100-annotations/EPIC_100_train.csv \
-   --out_folder /storage-rcp-pure/upmwmathis_scratch/shaokai/EK100_inst_train \
-   --avion_train_predictions /storage-rcp-pure/upmwmathis_scratch/shaokai/avion_predictions_train.json \
+   --train_metadata /capstor/scratch/cscs/hqi/llava/EK100/epic-kitchens-100-annotations/EPIC_100_train.csv \
+   --out_folder /capstor/scratch/cscs/hqi/llava/EK100/EK100_inst_train \
+   --avion_train_predictions /capstor/scratch/cscs/hqi/llava/EK100/avion_predictions_train.json \
    --gen_type avion_mc \
    --n_options 10
 
