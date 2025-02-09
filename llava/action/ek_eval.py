9575db4e0eb7e0c1a2e1c628de4556f6f4a443ae
Ye Shaokai <sye@haas001.ds-a4-r02.cct.rcp.epfl.ch>
2025-02-09 10:44:58 +0100

    updates

--- llava/action/ek_eval.py
+++ llava/action/ek_eval.py
@@ -342,7 +342,10 @@ def evaluate_on_EK100(eval_args,
     if eval_args.learn_neighbor_actions:
         from llava.action.generate_interval_pred import  get_lookup_dict
         if eval_args.test_type.startswith('temporal_cot'):
-            lookup_table = get_lookup_dict(eval_args.val_metadata, test_type = eval_args.test_type, pseudo_folder = eval_args.pseudo_folder)
+            lookup_table = get_lookup_dict(eval_args.val_metadata, 
+                                           eval_args.action_representation,
+                                           test_type = eval_args.test_type, 
+                                           pseudo_folder = eval_args.pseudo_folder)
 
 
     for idx, (frames, mc_data, time_meta, global_index) in tqdm(enumerate(val_dataloader)):                  
